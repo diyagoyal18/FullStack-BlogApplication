99f6615528e4f46355bda4d4147abcb027c0ed17
Diya Goyal <dgoyal6_be22@thapar.edu>
2023-12-24 13:39:03 +0530

    connect to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 import express from "express";
-
+import mongoose from 'mongoose';
 const app= express();
 
-app.use("/",(req,res, next)=>{
-    res.send("helo");
-});
-
-app.listen(5000);
+mongoose.connect(
+    'mongodb+srv://dgoyal6be22:dK7hPyLKwbn69nP7@cluster0.m8lzqna.mongodb.net/Blog?retryWrites=true&w=majority'
+    ).then(()=>app.listen(5000)).then(()=>console.log("connected to database and listening to 5000")).catch((err)=>console.log("error"));
