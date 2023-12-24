c844c9b987e8c0b90e737e6ce0748af9a3d7ec8a
Diya Goyal <dgoyal6_be22@thapar.edu>
2023-12-24 22:26:43 +0530

    user

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
 import express from "express";
 import mongoose from 'mongoose';
+import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes";
+
 const app= express();
 
-mongoose.connect(
+app.use(express.json());
+app.use("/api/user",router);
+app.use("/api/blog",blogRouter);
+mongoose
+    .connect(
     'mongodb+srv://dgoyal6be22:dK7hPyLKwbn69nP7@cluster0.m8lzqna.mongodb.net/Blog?retryWrites=true&w=majority'
-    ).then(()=>app.listen(5000)).then(()=>console.log("connected to database and listening to 5000")).catch((err)=>console.log("error"));
+    )
+    .then(()=>app.listen(5000))
+    .then(()=>
+    console.log("connected to database and listening to 5000")
+    )
+    .catch((err)=>console.log("error"));
---
+++ backend/routes/blog-routes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { getAllBlogs} from "../controllers/blog-controller";
+const blogRouter = express.Router();
+
+blogRouter.get("/", getAllBlogs);
+export default blogRouter;
