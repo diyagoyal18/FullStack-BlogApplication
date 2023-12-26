db78b198d9907db34dfdc44e1315611f0ac49fea
Diya Goyal <dgoyal6_be22@thapar.edu>
2023-12-26 22:47:20 +0530

    backend final

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
 import express from "express";
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import router from "./routes/user-routes";
 import blogRouter from "./routes/blog-routes";
-
+dotenv.config();
 const app= express();
-
 app.use(express.json());
 app.use("/api/user",router);
 app.use("/api/blog",blogRouter); 
+const key = process.env.SECRET_KEY;
 mongoose
     .connect(
-    'mongodb+srv://dgoyal6be22:dK7hPyLKwbn69nP7@cluster0.m8lzqna.mongodb.net/Blog?retryWrites=true&w=majority'
+    'mongodb+srv://dgoyal6be22:'+key+'@cluster0.m8lzqna.mongodb.net/Blog?retryWrites=true&w=majority'
     )
     .then(()=>app.listen(5000))
     .then(()=>
